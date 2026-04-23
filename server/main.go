ac1668635f83234451721443a9c536312d1f5c4e
Prateet Tiwari <prateettiwari29@gmail.com>
2026-04-23 21:22:09 +0530

    improve logging in main function

--- server/main.go
+++ server/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc(
 		"/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Let's Go!", r.URL.Path)
+			fmt.Fprintln(w, "Let's Go!")
 		})
 
-	http.ListenAndServe(":80", nil)
+	fmt.Println("Server is runnig on port 80")
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
