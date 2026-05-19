244b7b79835f921a42bcaac1eea8bbf45e361ccd
Prateet Tiwari <prateettiwari29@gmail.com>
2026-05-20 02:30:35 +0530

    Add initial implementation of main function with variable declarations and print statements in Go

---
+++ base-v2/var/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// name := "Yash" // invalid globally
+
+func main() {
+
+	name := "Yash"
+	var age int = 20
+	var height = 180
+
+	var lastName string
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(height)
+	fmt.Println(lastName)
+}
