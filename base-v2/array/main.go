2d67c584f69ce8d3108f6f468053497a21715eaa
Prateet Tiwari <prateettiwari29@gmail.com>
2026-05-21 21:31:25 +0530

    Add initial implementation of array handling in main function with print statements in Go

---
+++ base-v2/array/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [5]int
+	arr[0] = 10
+	arr[1] = 20
+	arr[2] = 30
+	arr[3] = 40
+	arr[4] = 50
+
+	fmt.Println(arr)
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+}
