56cf1166f3a7a71fe35a37b7808a950e353229a4
Prateet Tiwari <prateettiwari29@gmail.com>
2026-05-23 21:39:19 +0530

    Add initial implementation of main function and utility for addition

---
+++ base-v2/package/go.mod
@@ -0,0 +1,3 @@
+module go-lab/base-v2/package
+
+go 1.25.0
---
+++ base-v2/package/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"go-lab/base-v2/package/utils"
+)
+
+func main() {
+	fmt.Println(utils.Add(10, 20))
+}
