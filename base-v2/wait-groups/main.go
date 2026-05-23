fb46092560dccc498db53432e6260f9fd1f698f9
Prateet Tiwari <prateettiwari29@gmail.com>
2026-05-23 21:08:59 +0530

    Add initial implementation of wait groups with worker function and main execution

---
+++ base-v2/wait-groups/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println("Worker", id, "done")
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 1000; i++ {
+		wg.Add(1)
+
+		go worker(i, &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println("All workers completed")
+}
