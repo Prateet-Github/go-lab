75eb1a09d9282188657e7dbb193b94655cffaa86
Prateet Tiwari <prateettiwari29@gmail.com>
2026-07-16 19:37:34 +0530

    Update users route response to include microservice designation

--- backend/backend.go
+++ backend/backend.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello from users-service")
+		fmt.Fprintln(w, "Hello from users-service (microservice)!")
 	})
 
 	http.ListenAndServe(":9000", nil)
